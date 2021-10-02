63754360a6e7a56525104cfd815fcba6967b52ed
Francisco <frankmartinez4050@gmail.com>
2021-10-02 14:44:09 -0500

    partially done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-document.querySelector('#github-form').addEventListener('click', (e) => {
-    console.log(e)
+document.addEventListener('DOMContentLoaded', () => {
+document.getElementById('github-form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        let search = document.querySelector('#search')
+        fetch(`https://api.github.com/search/users?q=${search.value}`, {
+            headers: {
+                'Content-Type': "application/json",
+                accept: 'application/vnd.github.v3+json'
+            }
+        })
+        .then(res => res.json())
+        .then(name => {
+            console.log(name.items)
+            search.value
+            let appendValues = name.items[0].login
+            document.querySelector('#github-container').append(appendValues)
+        }
+    )})
 })
