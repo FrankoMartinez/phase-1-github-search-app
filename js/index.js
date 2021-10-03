bc2ebf39ed36e2997b3aa6a394a568418b7ace22
Francisco <frankmartinez4050@gmail.com>
2021-10-03 12:38:50 -0500

    commit tree d158ddd3020b6c0f19976349670dbd4579fa44d3

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// let search = document.querySelector('#search')
-
 document.addEventListener('DOMContentLoaded', () => {
 document.getElementById('github-form').addEventListener('submit', (e) => {
         e.preventDefault()
@@ -14,21 +12,34 @@ document.getElementById('github-form').addEventListener('submit', (e) => {
         .then(object => {
             object.items.forEach(name => appendGitHubProfile(name))
         }
-    )})
+    )}
+    )
 })
 
 function appendGitHubProfile(name) {
-    console.log(name)
-    console.log("Test")
-   let githubContainer = document.querySelector('#github-container')
+   let githubUser = document.createElement('li')
+   let userList = document.getElementById('user-list')
+   let h1 = document.createElement('h1')
+   let img = document.createElement('img')
+   let a = document.createElement('a')
 
-let githubUser = `
-<div>
-    <h1>${name.login}</h1>
-    <img src = ${name.avatar_url}</img> 
-    <a href=${name.html_url}>Github Profile</a>
-    <p> ${name.score} </p>
-</div>`
-
-   githubContainer.innerHTML += githubUser
+   h1.innerText = name.login
+   a.href = name.html_url
+   a.innerText = 'Github Profile'
+   img.src = name.avatar_url
+    let br = document.createElement('br')
+    githubUser.append(h1, br, a, br, img,) 
+    userList.appendChild(githubUser)
+    h1.addEventListener('click', () => {
+        fetch(`https://api.github.com/users/${name.login}/repos`, {
+            headers: {
+                'Content-Type': "application/json",
+                Accept: 'application/vnd.github.v3+json'
+            }
+        })
+        .then(res => res.json())
+        .then(e => {
+            console.log(e)
+        })
+    })
 }
